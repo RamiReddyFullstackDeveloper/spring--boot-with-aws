d3497615404c639ccfd47e1ec10f5e450c024766
ramireddy <ramireddyvakamalla@gmail.com>
2024-09-01 16:05:40 +0530

    env

--- spring-boot-with-aws/src/main/java/com/aws/s3/eventbridge/ecs/controller/AWSS3Controller.java
+++ spring-boot-with-aws/src/main/java/com/aws/s3/eventbridge/ecs/controller/AWSS3Controller.java
@@ -3,20 +3,31 @@ package com.aws.s3.eventbridge.ecs.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aws.s3.eventbridge.ecs.util.EmployeeUtil;
 
+
+import org.springframework.beans.factory.annotation.Value;
+
 @RestController
 @RequestMapping("/s3")
 public class AWSS3Controller {
 	
+	@Value("${employee.name}")
+	private String name;
 	
 	
-	@GetMapping("/emp")
-	public ResponseEntity<Object> getEmployee() {
-		return  ResponseEntity.status(HttpStatus.OK).body(EmployeeUtil.getEmp());
+	@GetMapping("/emp/{pathName}")
+	public ResponseEntity<Object> getEmployee(@PathVariable String pathName) {
+		return  ResponseEntity.status(HttpStatus.OK).body(EmployeeUtil.getEmp(pathName));
+	}
+
+	@GetMapping("emp/name")
+	public ResponseEntity<String> getName(){
+		return ResponseEntity.status(200).body(name);
 	}
 
 }
--- spring-boot-with-aws/src/main/java/com/aws/s3/eventbridge/ecs/util/EmployeeUtil.java
+++ spring-boot-with-aws/src/main/java/com/aws/s3/eventbridge/ecs/util/EmployeeUtil.java
@@ -4,9 +4,9 @@ import com.aws.s3.eventbridge.ecs.model.Employee;
 
 public class EmployeeUtil {
 	
-	public static Employee getEmp() {
+	public static Employee getEmp(String name) {
 		
-		return new Employee("Rami Reddy", "7396126247", "02-07-1991", "Module Lead");
+		return new Employee(name, "7396126247", "02-07-1991", "Module Lead");
 	}
 
 }
